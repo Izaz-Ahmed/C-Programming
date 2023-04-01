a7c7a2d89f3699e0263bebd9b87d917751d6b7ab
Izaz-Ahmed <ahmedizaz414@gmail.com>
2023-04-01 17:38:50 +0600

    new setup

---
+++ setup/1st_code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  /**
+  int n;
+  scanf("%d", &n);
+  if(n == 1) printf("This is the book about which there is no doubt.");
+  else printf("Good boy");
+
+  */
+
+ printf("I love Bangladesh. Bangladesh is my homeland.");
+
+
+  return 0;
+}
